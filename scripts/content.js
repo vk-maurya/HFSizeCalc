0b9ca039f297dde307d3b12211824fc4c77d8d38
vmaurya <vijay.maurya@morningstar.com>
2023-09-16 17:51:51 +0530

    loading repo calc Feature added

--- scripts/content.js
+++ scripts/content.js
@@ -26,31 +26,63 @@ function calculateTotalBinFileSizeInGB(html,fil_ext) {
     }
     return totalSizeInGB;
 }
-if (document.querySelector('.mr-4.flex.min-w-0.basis-auto.flex-wrap.items-center.md\\:flex-grow.md\\:basis-full.lg\\:basis-auto.lg\\:flex-nowrap')) {
-    const html = document.documentElement.innerHTML;
-    const bin_size = calculateTotalBinFileSizeInGB(html,'bin').toFixed(2);
-    const safetensors_size = calculateTotalBinFileSizeInGB(html,'safetensors').toFixed(2);
-    const pth_size    = calculateTotalBinFileSizeInGB(html,'pth').toFixed(2);
-
-    const fileSizes = [];
-
-    if (bin_size > 0) {
-        fileSizes.push(`.bin size: ${bin_size}`);
+async function clickLoadMoreButton() {
+    // Select the "Load More" button using its class name with colons escaped
+    const loadMoreButton = document.querySelector('.flex.flex-grow.items-center.justify-center.group-hover\\:underline');
+  
+    if (loadMoreButton) {
+      // Trigger a click event on the button
+      loadMoreButton.click();
+      await new Promise(resolve => setTimeout(resolve, 100)); // Wait for 100 milliseconds
+      await clickLoadMoreButton(); // Click again after waiting
+    } else {
+      console.log("No more 'Load More' buttons found.");
     }
-    
-    if (safetensors_size > 0) {
-        fileSizes.push(`.safetensors size: ${safetensors_size}`);
-    }
-    
-    if (pth_size > 0) {
-        fileSizes.push(`.pth size: ${pth_size}`);
-    }
-    
-    const outputString = fileSizes.join(' GB | ');
-
-
-    const message = `<div> <div><div id="size" class="mb-2 flex items-center overflow-hidden mt-0.5 ml-1.5 rounded border border-yellow-200 bg-yellow-50 px-1 text-xs font-semibold uppercase text-yellow-500 dark:bg-yellow-800 dark:text-yellow-400" style="color=red">   weight file size is : ${outputString} GB </div>`;
-    const divElement = document.querySelector('.mr-4.flex.min-w-0.basis-auto.flex-wrap.items-center.md\\:flex-grow.md\\:basis-full.lg\\:basis-auto.lg\\:flex-nowrap');
-
+  }
+  
+  async function main() {
+    const selector = '.mr-4.flex.min-w-0.basis-auto.flex-wrap.items-center.md\\:flex-grow.md\\:basis-full.lg\\:basis-auto.lg\\:flex-nowrap';
+    if (document.querySelector(selector)) {
+      await clickLoadMoreButton(); // Click "Load More" buttons sequentially
+  
+      // After clicking all buttons, perform your calculations
+      const html = document.documentElement.innerHTML;
+      const bin_size = calculateTotalBinFileSizeInGB(html, 'bin').toFixed(2);
+      const safetensors_size = calculateTotalBinFileSizeInGB(html, 'safetensors').toFixed(2);
+      const pth_size = calculateTotalBinFileSizeInGB(html, 'pth').toFixed(2);
+  
+      const fileSizes = [];
+  
+      if (bin_size > 0) {
+        fileSizes.push(`.bin size: ${bin_size} GB`);
+      }
+  
+      if (safetensors_size > 0) {
+        fileSizes.push(`.safetensors size: ${safetensors_size} GB`);
+      }
+  
+      if (pth_size > 0) {
+        fileSizes.push(`.pth size: ${pth_size} GB`);
+      }
+  
+      const outputString = fileSizes.join(' | ');
+  
+      const message = `<div id="size" class="mb-2 flex items-center overflow-hidden mt-0.5 ml-1.5 rounded border border-yellow-200 bg-yellow-50 px-1 text-xs font-semibold uppercase text-yellow-500 dark:bg-yellow-800 dark:text-yellow-400" style="color: #f59e0b;">Weight file size is: ${outputString}</div>`;
+  
+      const existingSizeDiv = document.querySelector('#size');
+      const divElement = document.querySelector(selector);
+  
+      if (existingSizeDiv) {
+        // If the element with id "size" already exists, replace it
+        existingSizeDiv.outerHTML = message;
+      } else {
+        // If the element doesn't exist, append the new element
         divElement.innerHTML += message;
-}
+      }
+    } else {
+        //DO Nothing
+    }
+  }
+  
+  main(); // Start the main function
+  
