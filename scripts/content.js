86b3ccd47c75f159f78e835caaa63645728ef139
Vijay Maurya <Vijay.Maurya@morningstar.com>
2023-12-31 09:39:44 +0530

    regex code update

--- scripts/content.js
+++ scripts/content.js
@@ -1,30 +1,30 @@
 function convertFileSizeToGB(sizeText) {
-    const sizeMatch = sizeText.match(/^([\d.]+) (GB|Bytes|KB|MB)$/);
-    if (sizeMatch && sizeMatch.length === 3) {
-        const size = parseFloat(sizeMatch[1]);
-        const unit = sizeMatch[2];
-        if (unit === 'Bytes') {
-            return size / 1024 / 1024 / 1024;
-        } else if (unit === 'KB') {
-            return size / 1024 / 1024;
-        } else if (unit === 'MB') {
-            return size / 1024;
-        } else if (unit === 'GB') {
-            return size;
-        }
-    }
-    return 0;
+  const sizeMatch = sizeText.match(/^([\d.]+) (GB|Bytes|KB|MB)$/);
+  if (sizeMatch && sizeMatch.length === 3) {
+      const size = parseFloat(sizeMatch[1]);
+      const unit = sizeMatch[2];
+      if (unit === 'Bytes') {
+          return size / 1024 / 1024 / 1024;
+      } else if (unit === 'KB') {
+          return size / 1024 / 1024;
+      } else if (unit === 'MB') {
+          return size / 1024;
+      } else if (unit === 'GB') {
+          return size;
+      }
+  }
+  return 0;
 }
 
-function calculateTotalBinFileSizeInGB(html,fil_ext) {
-    const binFileRegex = new RegExp(`\\/[^/]+\\.${fil_ext}"\\s*>([\\d.]+ (GB|Bytes|KB|MB))`, 'g');
+function calculateTotalBinFileSizeInGB(html, fil_ext) {
+  const binFileRegex = new RegExp(`\\.${fil_ext}\\?download=true">([\\d.]+) (GB|Bytes|KB|MB)`, 'g');
 
-    let totalSizeInGB = 0;
-    let match;
-    while ((match = binFileRegex.exec(html)) !== null) {
-        totalSizeInGB += convertFileSizeToGB(match[1]);
-    }
-    return totalSizeInGB;
+  let totalSizeInGB = 0;
+  let match;
+  while ((match = binFileRegex.exec(html)) !== null) {
+      totalSizeInGB += convertFileSizeToGB(`${match[1]} ${match[2]}`);
+  }
+  return totalSizeInGB;
 }
 async function clickLoadMoreButton() {
     // Select the "Load More" button using its class name with colons escaped
@@ -45,26 +45,21 @@ async function clickLoadMoreButton() {
     if (document.querySelector(selector)) {
       await clickLoadMoreButton(); // Click "Load More" buttons sequentially
   
-      // After clicking all buttons, perform your calculations
-      const html = document.documentElement.innerHTML;
-      const bin_size = calculateTotalBinFileSizeInGB(html, 'bin').toFixed(2);
-      const safetensors_size = calculateTotalBinFileSizeInGB(html, 'safetensors').toFixed(2);
-      const pth_size = calculateTotalBinFileSizeInGB(html, 'pth').toFixed(2);
-  
-      const fileSizes = [];
-  
-      if (bin_size > 0) {
-        fileSizes.push(`.bin size: ${bin_size} GB`);
-      }
-  
-      if (safetensors_size > 0) {
-        fileSizes.push(`.safetensors size: ${safetensors_size} GB`);
-      }
-  
-      if (pth_size > 0) {
-        fileSizes.push(`.pth size: ${pth_size} GB`);
-      }
-  
+      // Extract file sizes
+      const bin_size = calculateTotalBinFileSizeInGB(document.documentElement.innerHTML, 'bin').toFixed(2);
+      const safetensors_size = calculateTotalBinFileSizeInGB(document.documentElement.innerHTML, 'safetensors').toFixed(2);
+      const pth_size = calculateTotalBinFileSizeInGB(document.documentElement.innerHTML, 'pth').toFixed(2);
+      const pt_size = calculateTotalBinFileSizeInGB(document.documentElement.innerHTML, 'pt').toFixed(2);
+
+      // Create an array of file size strings
+      const fileSizes = [
+        bin_size > 0 && `.bin size: ${bin_size} GB`,
+        pt_size > 0 && `.pt size: ${pt_size} GB`,
+        safetensors_size > 0 && `.safetensors size: ${safetensors_size} GB`,
+        pth_size > 0 && `.pth size: ${pth_size} GB`,
+      ].filter(Boolean); // Filter out falsy values
+
+      // Create the output string
       const outputString = fileSizes.join(' | ');
   
       const message = `<div id="size" class="mb-2 flex items-center overflow-hidden mt-0.5 ml-1.5 rounded border border-yellow-200 bg-yellow-50 px-1 text-xs font-semibold uppercase text-yellow-500 dark:bg-yellow-800 dark:text-yellow-400" style="color: #f59e0b;">Weight file size is: ${outputString}</div>`;
